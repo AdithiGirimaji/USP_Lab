ca90ab2f67cbf294f774b1d7e000489902ecb4ae
AdithiGirimaji <71318795+AdithiGirimaji@users.noreply.github.com>
2022-01-08 20:41:40 +0530

    Update eighteen.c

--- week5/eighteen.c
+++ week5/eighteen.c
@@ -6,7 +6,7 @@ int main(int argc, char * argv[])
 {
 if(argc < 3 || argc > 4 || (argc == 4 && strcmp(argv[1],"-s")))
 {
-printf("Usage: ./a.out [-s] <org_file> <new_link>;\n");
+printf("Usage: ./a.out [-s] <org_file> <new_link>\n");
 return 1;
 }
 if(argc == 4)
